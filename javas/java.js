18181b2154f34e5b82466757f7be00debc0490e9
kamrie <ifedikeze@gmail.com>
2023-07-07 22:43:23 +0100

    first commit

---
+++ javas/java.js
@@ -0,0 +1,45 @@
+
+
+
+// // RENDERING OUT ERROR MESSAGE ON THE SHOE//
+// // let seconderrorparagraph= document.getElementById("second-error")
+// // console.log (seconderrorparagraph)
+
+// // function purchase(){
+// //     // console.log("clicked button")
+// //     seconderrorparagraph.textContent=  "Something went wrong, please try again in the paragraph"
+// //     setTimeout(() => {
+// //         seconderrorparagraph.textContent= ""
+// //     }, 1500)
+// // }
+
+
+//  value.toString().slice(0, -1)
+// // 
+
+// let sumEl= document.getElementById("sum-el")
+
+// function add() {
+//     // let result= firstNum +secondNum 
+//     // sumEl.textContent= "Sum:"+ result
+//     if(result === 0){
+//        console.log(firstNum + secondNum) 
+//     }
+// }
+// function subtract(){
+//     let result= firstNum-secondNum
+//     sumEl.textContent=  "Sum:"+ result
+// }
+// function divide(){
+//     let result= firstNum/secondNum
+//     sumEl.textContent=  "Sum:"+ result
+// }
+// function multiply(){
+//     let result= firstNum*secondNum
+//     sumEl.textContent=  "Sum:"+ result
+// }
+
+
+
+
+
